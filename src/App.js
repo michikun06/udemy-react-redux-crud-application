b9a38d5a9c2accad1e6c02f0897943773a49597a
kawaji michihiro <michikun06@icloud.com>
2020-06-25 13:22:03 +0900

    learn props and component

--- src/App.js
+++ src/App.js
@@ -12,20 +12,26 @@ import React from 'react';
 // }
 
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 }
+  ]
+
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return (
+            <User name={profile.name} age={profile.age} key={index} />
+          );
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Nya!</div>
+const User = (props) => {
+  return <div>Hi!, I am {props.name}, and {props.age} years old.</div>
 }
 
 export default App;
