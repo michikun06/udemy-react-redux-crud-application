ee3d688f692fff83e1f9cfec4f4f9cd2690a0029
kawaji michihiro <michikun06@icloud.com>
2020-06-30 14:44:47 +0900

    implement delete feature

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,12 +1,27 @@
 import _ from 'lodash';
 
-import { READ_EVENTS } from "../actions";
+import {
+    CREATE_EVENTS,
+    READ_EVENT,
+    READ_EVENTS,
+    UPDATE_EVENTS,
+    DELETE_EVENTS
+} from "../actions";
 
 export default (events = {}, action) => {
     switch (action.type) {
+        case CREATE_EVENTS:
+        case READ_EVENT:
+        case UPDATE_EVENTS:
+            const data = action.response.data
+            return { ...events, [data.id]: data }
         case READ_EVENTS:
             // action.response.dataのidをkeyとして配列を作成（keyの番号を呼び出すだけで情報の取得が可能）
             return _.mapKeys(action.response.data, 'id')
+        case DELETE_EVENTS:
+            console.log(action.id)
+            delete events[action.id]
+            return { ...events }
         default:
             return events
     }
